09ee769f30806ddbd7a924b326a1a2c7345463a9
HaowenWeiJohn <hw2892@columbia.edu>
2023-08-22 01:56:51 +0800

    add game states

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public List<Preset.GameState> gameStates = new List<Preset.GameState>();
+    public int currentGameStaateIndex = 0;
+    public static KeyCode NextStateKey = KeyCode.N;
+    public static KeyCode InterruptKey = KeyCode.I;
+
+
+
     void Start()
     {
-        
+        gameStates = Preset.GameProcedure;
+        currentGameStaateIndex = 0;
+
+        // set current Game State
+
     }
 
     // Update is called once per frame
